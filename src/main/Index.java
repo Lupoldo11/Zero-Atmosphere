e29eb41291bbfb9da48a3f6f71553f69aac6620b
Lupoldo <alexhprogram@gmail.com>
2025-02-14 17:43:11 +0100

    Version 0.0

---
+++ src/main/Index.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ *
+ * @author Lupoldo
+ */
+public class Index {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
