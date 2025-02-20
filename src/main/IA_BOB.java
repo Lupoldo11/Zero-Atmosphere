76b3ee68a6f55c9dfe14093f204744f53190c19c
Lupoldo <alexhprogram@gmail.com>
2025-02-20 16:14:17 +0100

    Class Persona(subclases) + Alien(subclases) + Codigo + Mascota(subclase) + Habitaciones(subclases) Vehiculo(subclase) + IA_Bob + Text +  main version 0.5

--- src/main/IA_BOB.java
+++ src/main/IA_BOB.java
@@ -1,24 +1,35 @@
-//*) Genera los 4 principales tripulantes (Carmen, Laura, Lopez, federico) , la nave principal, la nave objetivo
-//IDEA: hacer un menu inicial: 1) Hace un escaner a la nave y genera todo automáticamente
-//                                             2) Salta la IA_Bob y va pidiendo datos y genera el resultado
 package main;
 /**
  *
  * @author SKTelecom
  */
 import java.util.Vector;
-public class IA_EVA {
+public class IA_BOB {
     
     //Genera los aliens que y los añade a la array 
     public static void generadorAlien (int total_alien, int contador_Raiser){
         for (int i=0;i<total_alien;i++){
                     if (contador_Raiser!=0){
                         Alien ejemplo1 = new Raiser(1,"prueba","prueba","prueba",2);
-                        Text.listAlien.add(ejemplo1); 
+                        Text.listAlien.add(ejemplo1);
                         contador_Raiser--;
                     } else {
                         Alien ejemplo2 = new Nomun(1,"prueba","prueba","prueba",2);
                         Text.listAlien.add(ejemplo2); 
+                    }
+        }
+    }
+    
+    //Genera las personas y añade al array
+    public static void generadorPersona (int total_humanos, int num_soldados){
+        for (int i=0;i<total_humanos;i++){
+                    if (num_soldados!=0){
+                        Persona ejemplo1 = new Soldado("prueba","prueba",2,2,"prueba");
+                        Text.listPersona.add(ejemplo1);
+                        num_soldados--;
+                    } else {
+                        Persona ejemplo2 = new Minero("prueba","prueba",2,2);
+                        Text.listPersona.add(ejemplo2); 
                     }
         }
     }
@@ -47,24 +58,11 @@ public class IA_EVA {
         
         //1)Num aliens (puede ser aleatorio o seleccion)
         System.out.println("Accion 1");
-        System.out.println("Opciones de escaner: \n1) Escaner automático \n2) IA_Bob");
-        String seleccion_num_alien = Text.intro.next();
-        int contador_Raiser=0, contador_Nomun=0, total_alien=0;
-
-        switch (seleccion_num_alien) {
-            case "1" -> {
-                contador_Raiser= (int) (Math.random() * 200) + 1; //Gaston funcion random (debe de modular)
-                contador_Nomun= (int) (Math.random() * 200) + 1; //Gastor funcion random (debe de modular)
-                total_alien = contador_Raiser + contador_Nomun;
-            }
-            case "2" -> {
-                total_alien = Text.intro.nextInt();
-                Text.intro.nextLine();
-                contador_Raiser= (int) (Math.random() * total_alien) + 1; //Gaston funcion random (debe de modular)
-                contador_Nomun= (int) (Math.random() * total_alien) + 1; //Gastor funcion random (debe de modular)
-            }
-            default -> System.out.println(Text.error_seleccion);
-        }
+        int contador_Raiser, contador_Nomun, total_alien;
+        total_alien = Text.intro.nextInt();
+        Text.intro.nextLine();
+        contador_Raiser= (int) (Math.random() * total_alien) + 1; //Gaston funcion random (debe de modular)
+        contador_Nomun= total_alien-contador_Raiser; //Gastor funcion random (debe de modular)
         //enlaza a la funcion de generarAlien
         generadorAlien(total_alien,contador_Raiser);
         
@@ -76,9 +74,18 @@ public class IA_EVA {
         System.out.println("La distancia con la nave es de: "+distancia+"año/luz");
         
         //3)Lista soldados (nombres - toString) a intervenir (pedir a usuario)
-        System.out.println("Accion 3");
+        System.out.println("Accion ");
         //4)Lista mineros (nombres - toString) a intervenir (pedir a usuario)
-        System.out.println("Accion 4");
+        System.out.println("Accion 3 y 4");
+        System.out.println("¿Cuántos soldados quieres?");
+        int num_soldados = Text.intro.nextInt();
+        System.out.println("¿Cuántos mineros quieres?");
+        int num_mineros = Text.intro.nextInt();
+        int total_personas= num_soldados + num_mineros;
+        
+        generadorPersona(total_personas,num_soldados);
+        
+        System.out.println("Tamaño lista: "+Text.listPersona.size());
         for(int i=0; i<Text.listPersona.size();i++){ //esto se puede cambiar por el modulo=> metodo_toString()
             Persona ejemplo = Text.listPersona.get(i);
             if (ejemplo instanceof Soldado){
@@ -91,15 +98,7 @@ public class IA_EVA {
         
         //6)Lista de aliens por tipos encontrados (toString de funciones de arriba)
         System.out.println("Accion 6");
-        for(int i=0; i<Text.listAlien.size();i++){//esto se puede cambiar por el modulo=> metodo_toString()
-            Alien ejemplo = Text.listAlien.get(i);
-            if (ejemplo instanceof Raiser){
-                System.out.println(Text.listAlien.get(i).toString());
-            }
-            if (ejemplo instanceof Nomun) {
-                System.out.println(Text.listAlien.get(i).toString());
-            }
-        }
+        System.out.println("Listado de Alien: "+Text.listAlien.size() +"\nNúmero de Nomun: "+contador_Nomun + "\nNúmero de Raiser: "+contador_Raiser);
         
         //7)Vehiculos utilizados  (formula antia de empaquetamiento)
         System.out.println("Accion 7");
@@ -108,22 +107,5 @@ public class IA_EVA {
         System.out.println("Accion 8");
         double coste_total=0; //Aquí va la función de ana del SUMATORIO
         System.out.println("El coste total del operativo es de: " + coste_total);
-    }
-    
-    public static void main(String[] args) {
-        boolean salida=false;
-        do {
-            System.out.println(Text.menu_seleccion_inicio);
-            String seleccion = Text.intro.next();
-            
-            switch (seleccion) {
-                case "1" -> calculador();
-                case "2" -> salida=true;
-                default -> System.out.println(Text.error_seleccion);
-            }
-        } while (salida!=true);
-        
-        System.out.println(Text.error_seleccion);
-    }
-    
+    }      
 }
--- src/main/Raiser.java
+++ src/main/Raiser.java
@@ -51,6 +51,7 @@ public class Raiser extends Alien {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
+   @Override
     public String toString(){
     return "raisers";
     }
