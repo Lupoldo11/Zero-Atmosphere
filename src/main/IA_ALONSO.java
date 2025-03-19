b94e196e91154d674ab2503897a641ba1eac4d02
eva <emsz45@educa.madrid.com>
2025-03-19 18:08:47 +0100

    como me da error, en la clase vehiculo he dejado comentado para mostrar

--- src/main/IA_ALONSO.java
+++ src/main/IA_ALONSO.java
@@ -11,57 +11,63 @@ import aliens.Alien;
 import personas.Persona;
 import vehiculos.Vehiculo;
 
+
 public class IA_ALONSO {
             
     //Funcionamiento principal código (MAIN)
     public static void calculadorAutomatico(){
         
-        //1)Num aliens (aleatorio)
+         // 1) Num aliens (aleatorio)
         System.out.println(Text.guiones);
-        System.out.println(Text.accion+" 1");
+        System.out.println(Text.accion + " 1");
         int contador_Raiser, contador_Nomun, total_alien;
-        
-        contador_Raiser= (int) (Math.random() * 200) + 1; //Gaston funcion random (debe de modular)
-        contador_Nomun= (int) (Math.random() * 200) + 1; //Gastor funcion random (debe de modular)
+
+        contador_Raiser = (int) (Math.random() * 200) + 1; // Gaston funcion random (debe de modular)
+        contador_Nomun = (int) (Math.random() * 200) + 1; // Gaston funcion random (debe de modular)
         total_alien = contador_Raiser + contador_Nomun;
-        
-        //enlaza a la funcion de generarAlien
-        Alien.generadorAlien(total_alien,contador_Raiser);
-        
-        //6)Lista de aliens por tipos encontrados (toString de funciones de arriba)
+
+        // Enlaza a la funcion de generarAlien
+        Alien.generadorAlien(total_alien, contador_Raiser);
+
+        // 6) Lista de aliens por tipos encontrados (toString de funciones de arriba)
         System.out.println(Text.guiones);
-        System.out.println(Text.accion+" 6");
-        System.out.println(Text.listado_de+" Alien: "+Text.listAlien.size() + Text.numero_de+"\n Nomun: "+contador_Nomun + Text.numero_de+"\nRaiser: "+contador_Raiser);
-        
-        //2)Distancia nave
+        System.out.println(Text.accion + " 6");
+        System.out.println(Text.listado_de + " Alien: " + Text.listAlien.size() + Text.numero_de + "\n Nomun: " + contador_Nomun + Text.numero_de + "\nRaiser: " + contador_Raiser);
+
+        // 2) Distancia nave
         System.out.println(Text.guiones);
-        System.out.println(Text.accion+" 2");
+        System.out.println(Text.accion + " 2");
         System.out.print(Text.pregunta_distancia);
-        double distancia = (int) (Math.random() * 200) + 1;//Añadir ramdon de distancia
-        System.out.println(Text.respuesta_distancia+Text.distancia+Text.anios_luz+Vehiculo.metodoTransmision(Text.distancia));
-        
-        //Días
-        double dias_mision = (int) Math.floor(Text.distancia*3);
-        System.out.println(Text.duracion_mision+dias_mision+Text.dias);
-        
-        //3)Lista soldados (nombres - toString) a intervenir (pedir a usuario)
-        //4)Lista mineros (nombres - toString) a intervenir (pedir a usuario)
+        double distancia = (int) (Math.random() * 200) + 1; // Añadir random de distancia
+        System.out.println(Text.respuesta_distancia + Text.distancia + Text.anios_luz + Vehiculo.metodoTransmision(Text.distancia));
+
+        // Días
+        double dias_mision = (int) Math.floor(Text.distancia * 3);
+        System.out.println(Text.duracion_mision + dias_mision + Text.dias);
+
+        // 3) Lista soldados (nombres - toString) a intervenir (pedir a usuario)
+        // 4) Lista mineros (nombres - toString) a intervenir (pedir a usuario)
         System.out.println(Text.guiones);
-        System.out.println(Text.accion+" 3 y 4");
-        int num_soldados = total_alien*2;
+        System.out.println(Text.accion + " 3 y 4");
+        int num_soldados = total_alien * 2;
         int num_mineros = total_alien;
-        int total_personas= num_soldados + num_mineros;
-        Persona.generadorPersona(total_personas,num_soldados);
+        int total_personas = num_soldados + num_mineros;
+        Persona.generadorPersona(total_personas, num_soldados);
         IA_EVA.metodo_toString(Text.listPersona);
-        
-        //7)Vehiculos utilizados  (formula antia de empaquetamiento)
+
+        // 7) Vehículos utilizados (mostrar vehículos)
         System.out.println(Text.guiones);
-        System.out.println(Text.accion+" 7");
-        utils.AsignacionVehiculo.selectorVehiculos(Text.listPersona.size(), Text.listPersona); //EN PROCESO
-        
-        //8) Coste Total Operación (formula ana)
+        System.out.println(Text.accion + " 7");
+
+        // Mostrar los vehículos
+        System.out.println("Vehículos utilizados:");
+        for (Vehiculo vehiculo : Text.listVehiculo) {
+            System.out.println(vehiculo.toString());
+        }
+
+        // 8) Coste Total Operación (formula ana)
         System.out.println(Text.guiones);
-        System.out.println(Text.accion+" 8");
-        System.out.println(Text.coste_mision+ Sumatorio.costeTotal());
+        System.out.println(Text.accion + " 8");
+        System.out.println(Text.coste_mision + Sumatorio.costeTotal());
     }
 }
--- src/vehiculos/Vehiculo.java
+++ src/vehiculos/Vehiculo.java
@@ -32,8 +32,9 @@ public abstract class Vehiculo {
     }
     
     public double calculaCarburante(double distancia) {
-        return Math.sqrt(distancia) * gastoCarburante;
-    }
+    return Math.sqrt(distancia) * gastoCarburante;
+}
+
 
     //Metodo de Economia de distancia
     public static String metodoTransmision(double distancia) {
@@ -49,5 +50,38 @@ public abstract class Vehiculo {
     public String toString() {
         return Text.vehiculo_tj + turbojet + Text.str_codigo + codigo + '}';
     }
-
+//METODO PARA GENERAR VEHICULOS
+    /*
+    public static void generadorVehiculo(int total_vehiculos, int contador_AeroBike, int contador_AeroCar, int contador_Nave, int contador_TurboJet) {
+    for (int i = 0; i < total_vehiculos; i++) {
+      
+        if (contador_AeroBike > 0) {
+            
+            Vehiculo ejemplo1 = new AeroBike(2456,"rojo",true);  // Crear un AeroBike con personas
+            Text.listVehiculos.add(ejemplo1);
+            contador_AeroBike--;  // Decrementar el contador de AeroBike
+        }
+  
+        else if (contador_AeroCar > 0) {
+     
+            Vehiculo ejemplo2 = new AeroCar();  // Crear un AeroCar con personas
+            Text.listVehiculos.add(ejemplo2);
+            contador_AeroCar--;  // Decrementar el contador de AeroCar
+        }
+  
+        else if (contador_Nave > 0) {
+            // Crear una Nave (parámetros pueden ser ajustados según el tipo de nave)
+            
+            Vehiculo ejemplo3 = new Nave(2, 1000.0, 4, laboratorio, tunelCiclonico, 10, 200, TurboJet.motorNave(), Codigo.generarVehiculo());
+            Text.listVehiculos.add(ejemplo3);
+            contador_Nave--;  // Decrementar el contador de Naves
+        }
+      
+        else if (contador_TurboJet > 0) {
+            Vehiculo ejemplo4 = new TurboJet();
+            Text.listVehiculos.add(ejemplo4);
+            contador_TurboJet--;  // Decrementar el contador de TurboJets
+        }
+    }
+}*/
 }
